129620c03e945492ace56a6c024825d82fd408b6
Anthony Fearn <anthony.fearn@vima.uk>
2022-05-23 08:36:10 +0100

    Added Full Version

--- src/Core.php
+++ src/Core.php
@@ -6,6 +6,12 @@ class Core
 {
     public static function version()
     {
-	return 'Version 4.0';
+		return 'Version 4.0.0-Beta';
     }
+
+    public static function fullVersion()
+    {
+		return 'RedKuri Framework Version 4.0.0-Beta';
+    }
+
 }
